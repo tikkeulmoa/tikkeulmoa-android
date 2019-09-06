c2f32e289165f5858ec15fe5fe63778b89ebd283
suzzang <soochoi0325@gmail.com>
2019-09-06 20:48:08 +0900

    AccountDetail 추가 & Gradle 수정

---
+++ app/src/main/java/tikkeulmoa/cimile/org/ui/account/AccountDetailActivity.kt
@@ -0,0 +1,13 @@
+package tikkeulmoa.cimile.org.ui.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tikkeulmoa.cimile.org.R
+
+class AccountDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_detail)
+    }
+}
