814aa0b64b87a5afbf3f207f4720cb0b7f5bc25c
suzzang <soochoi0325@gmail.com>
2019-09-07 15:20:02 +0900

    커뮤니티 목표 설정 구현

---
+++ app/src/main/java/tikkeulmoa/cimile/org/ui/community/dialog/GoalDialog.kt
@@ -0,0 +1,23 @@
+package tikkeulmoa.cimile.org.ui.community.dialog
+
+import android.app.Activity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
+import kotlinx.android.synthetic.main.dialog_plus_member.*
+import tikkeulmoa.cimile.org.R
+import tikkeulmoa.cimile.org.ui.community.CommunityDetailActivity
+
+class GoalDialog(activity: Activity) : Dialog(activity) {
+
+    init {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setContentView(R.layout.dialog_plus_member)
+//        this.setCancelable(false) //뒤로가기,터치 무력화
+
+
+    }
+}
