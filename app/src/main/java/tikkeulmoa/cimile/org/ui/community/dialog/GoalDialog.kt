1b518d42d95ca07f6289e21b7d802b585b616e81
suzzang <soochoi0325@gmail.com>
2019-09-07 15:26:34 +0900

    수정

--- app/src/main/java/tikkeulmoa/cimile/org/ui/community/dialog/GoalDialog.kt
+++ app/src/main/java/tikkeulmoa/cimile/org/ui/community/dialog/GoalDialog.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.Window
+import kotlinx.android.synthetic.main.dialog_goal.*
 import kotlinx.android.synthetic.main.dialog_plus_member.*
 import tikkeulmoa.cimile.org.R
 import tikkeulmoa.cimile.org.ui.community.CommunityDetailActivity
@@ -15,8 +16,12 @@ class GoalDialog(activity: Activity) : Dialog(activity) {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        setContentView(R.layout.dialog_plus_member)
+        setContentView(R.layout.dialog_goal)
 //        this.setCancelable(false) //뒤로가기,터치 무력화
+
+        btn_goal_set.setOnClickListener {
+            dismiss()
+        }
 
 
     }
